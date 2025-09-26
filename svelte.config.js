d3d93a40f7b3472df91289084a5a21ea3b947e33
med2mad <mohamed.leghdaich@gmail.com>
2025-09-26 12:08:33 +0100

    debug

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			fallback: '404.html'
 		}),
 		prerender: {
 			entries: ['*']
 		}
 	}
 };
+
+export default config;
